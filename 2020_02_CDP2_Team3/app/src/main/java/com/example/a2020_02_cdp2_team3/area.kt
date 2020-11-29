4ed58288f398a69d7e5b2593c025d004b4efc06d
shhow1000 <shhow940425@gmail.com>
2020-11-29 16:08:29 +0900

    실시간함수삽입으로 api값변경

--- 2020_02_CDP2_Team3/app/src/main/java/com/example/a2020_02_cdp2_team3/area.kt
+++ 2020_02_CDP2_Team3/app/src/main/java/com/example/a2020_02_cdp2_team3/area.kt
@@ -45,8 +45,7 @@ class area : Fragment() {
         super.onStart()
         areaStart()
     }
-    private fun areaStart()
-    {
+    private fun areaStart() {
         StrictMode.enableDefaults()
         val search = view?.findViewById<Button>(R.id.searcharea)
         val Date = SimpleDateFormat("yyyyMMdd")
@@ -55,46 +54,43 @@ class area : Fragment() {
         val input = view?.findViewById<EditText>(R.id.inputarea)
         val status1 = view?.findViewById(R.id.result_area) as TextView
         val Allsearch = view?.findViewById<Button>(R.id.Allsearcharea)
-        val areacolor= view?.findViewById(R.id.areaColor) as TextView
+        val areacolor = view?.findViewById(R.id.areaColor) as TextView
 
 
+        var initem = false
+        var increateDt = false
+        var indeathCnt = false
+        var indefCnt = false
+        var ingubun = false
+        var inincDec = false
+        var inisolClrearCnt = false
+        var inisollngCnt = false
+        var inlocalOccCnt = false
+        var inoverFlowCnt = false
+        var inqurRate = false
+        var inseq = false
+        var instdDay = false
+        var inupdateDt = false
 
-
-
-            var initem = false
-            var increateDt = false
-            var indeathCnt = false
-            var indefCnt = false
-            var ingubun = false
-            var inincDec = false
-            var inisolClrearCnt = false
-            var inisollngCnt = false
-            var inlocalOccCnt = false
-            var inoverFlowCnt = false
-            var inqurRate = false
-            var inseq = false
-            var instdDay = false
-            var inupdateDt = false
-
-            var createDt: String? = null
-            var deathCnt: String? = null
-            var defCnt: String? = null
-            var gubun: String? = null
-            var incDec: String? = null
-            var isolClearCnt: String? = null
-            var isolingCnt: String? = null
-            var localOccCnt: String? = null
-            var overFlowCnt: String? = null
-            var qurRate: String? = null
-            var seq: String? = null
-            var stdDay: String? = null
-            var updateDt: String? = null
-        Allsearch?.setOnClickListener(){
+        var createDt: String? = null
+        var deathCnt: String? = null
+        var defCnt: String? = null
+        var gubun: String? = null
+        var incDec: String? = null
+        var isolClearCnt: String? = null
+        var isolingCnt: String? = null
+        var localOccCnt: String? = null
+        var overFlowCnt: String? = null
+        var qurRate: String? = null
+        var seq: String? = null
+        var stdDay: String? = null
+        var updateDt: String? = null
+        Allsearch?.setOnClickListener() {
             status1.setTextColor(Color.parseColor("#006400"))
             StrictMode.enableDefaults()
             try {
                 val url =
-                    URL("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=vTyNr6s%2FAnbthjWjdQwjXtaO4qcHv6fDx5WG76mYO0HNVlKx8lICPVvc6%2BYUg929269dQ3qX3srEyhqZvM%2F%2Fdw%3D%3D&pageNo=1&numOfRows=10&startCreateDt=$EndDate&endCreateDt=$EndDate")
+                        URL("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=vTyNr6s%2FAnbthjWjdQwjXtaO4qcHv6fDx5WG76mYO0HNVlKx8lICPVvc6%2BYUg929269dQ3qX3srEyhqZvM%2F%2Fdw%3D%3D&pageNo=1&numOfRows=10&startCreateDt=$EndDate&endCreateDt=$EndDate")
                 val paserCreator = XmlPullParserFactory.newInstance()
                 val parser = paserCreator.newPullParser()
                 parser.setInput(url.openStream(), null);
@@ -203,13 +199,13 @@ class area : Fragment() {
 
                         }
                         XmlPullParser.END_TAG -> {
-                            if (parser.name == "item"){
+                            if (parser.name == "item") {
 
-                                status1?.text ="""${status1?.text}
+                                status1?.text = """${status1?.text}
+  시/도         : $gubun
   신규확진자    : $localOccCnt 명
   등록일시      : $createDt
   사망자 수     : $deathCnt 명
-  시/도         : $gubun
   전일대비증감률 : $incDec %
   격리해제수     : $isolClearCnt 명
   10만명당발생률 : $qurRate %
@@ -245,7 +241,7 @@ class area : Fragment() {
             val inputresult = input?.text.toString()
             try {
                 val url =
-                    URL("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=vTyNr6s%2FAnbthjWjdQwjXtaO4qcHv6fDx5WG76mYO0HNVlKx8lICPVvc6%2BYUg929269dQ3qX3srEyhqZvM%2F%2Fdw%3D%3D&pageNo=1&numOfRows=10&startCreateDt=$EndDate&endCreateDt=$EndDate")
+                        URL("http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey=vTyNr6s%2FAnbthjWjdQwjXtaO4qcHv6fDx5WG76mYO0HNVlKx8lICPVvc6%2BYUg929269dQ3qX3srEyhqZvM%2F%2Fdw%3D%3D&pageNo=1&numOfRows=10&startCreateDt=$EndDate&endCreateDt=$EndDate")
                 val paserCreator = XmlPullParserFactory.newInstance()
                 val parser = paserCreator.newPullParser()
                 parser.setInput(url.openStream(), null);
@@ -354,24 +350,40 @@ class area : Fragment() {
 
                         }
                         XmlPullParser.END_TAG -> {
-                            if ((parser.name == "item")&&(inputresult==gubun)) {
-  var defCntNum= Integer.parseInt(localOccCnt.toString())
-  if((defCntNum==0)){
-      areacolor.setBackgroundColor(Color.parseColor("#7CFC00"))
-      areacolor.setText("상대적으로 안전한 지역입니다. 위험도 10% ")}
-                                if((defCntNum>0)&&(3<defCntNum)){
+                            if ((parser.name == "item") && (inputresult == gubun)) {
+                                var defCntNum = Integer.parseInt(localOccCnt.toString())
+                                if ((defCntNum == 0)) {
+                                    areacolor.setBackgroundColor(Color.parseColor("#7CFC00"))
+                                    areacolor.setText("신규확진자 0 명으로 집계되었습니다. 코로나 방침을 잘 준수해주시기 바랍니다." +
+                                            "  감염 위험도 10% ")
+                                }
+                                if ((defCntNum > 0) && (defCntNum < 3)) {
+                                    areacolor.setBackgroundColor(Color.parseColor("#7CFC00"))
+                                    areacolor.setText("신규확진자가 0명에서 3명사이로 집계되었습니다. 코로나 방침을 준수해주세요 .감염위험도 20% ")
+                                }
+                                if ((3 <= defCntNum) && (8 > defCntNum)) {
+                                    areacolor.setBackgroundColor(Color.parseColor("#F0E68C"))
+                                    areacolor.setText("신규확진자가 3명에서 8명사이로 집계되었습니다. 마스크는 필수입니다. 위험도 40%")
+                                }
+                                if ((8 <= defCntNum) && (20 > defCntNum)) {
                                     areacolor.setBackgroundColor(Color.parseColor("#FFFF00"))
-                                 areacolor.setText("소규모 확진자가 있습니다. 위험도 30%")}
-                                if((3<=defCntNum)&&(5>defCntNum)){  areacolor.setBackgroundColor(Color.parseColor("#FF6347"))
-                                    areacolor.setText("외출하실 때 마스크를 꼭 착용하세요. 위험도 60%")}
-                                if(5<=defCntNum){  areacolor.setBackgroundColor(Color.parseColor("#FF0000"))
-                                    areacolor.setText("오늘은 외출금지를 권장합니다. 위험도 80%")}
-                                status1?.text = """${status1?.text}
+                                    areacolor.setText("신규확진자가 8명에서 20명사이로 집계되었습니다. 지역내 집단감염의 위험이 있을 수도 있습니다. 위험도 60%")}
+                                    if ((20 <= defCntNum) && (50 > defCntNum)) {
+                                        areacolor.setBackgroundColor(Color.parseColor("#FF8C00"))
+                                        areacolor.setText("신규확진자가 20명에서 50명사이로 집계되었습니다. 매우 높은 수치이며 금일은 외출을 삼가하시는 것을 권장합니다. 위험도 75%")
+                                    }
+                                    if (50 < defCntNum) {
+                                        areacolor.setBackgroundColor(Color.parseColor("#FF0000"))
+                                        areacolor.setText("신규확진자가 50명이상으로 집계되었습니다. 지역내 코로나의 대유행을 나타냅니다. 외출을 절대 금합니다. 위험도 100%")
+                                    }
+
+                                     status1?.text =
+                                        """${status1?.text}
 -$gubun 지역의 실시간 코로나 현황입니다.-
+  시/도         : $gubun
   신규확진자    : $localOccCnt 명
   등록일시      : $createDt
   사망자 수     : $deathCnt 명
-  시/도         : $gubun
   전일대비증감률 : $incDec %
   격리해제수     : $isolClearCnt 명
   10만명당발생률 : $qurRate %
@@ -383,23 +395,23 @@ class area : Fragment() {
 -------------------------
               
 """
-                                initem = false
+                                        initem = false
+
+                                break
+                                }
                             }
                         }
-
-
+                        paserEvent = parser.next()
                     }
-                    paserEvent = parser.next()
+                } catch (e: XmlPullParserException) {
+                    status1?.text = "에러"
+                } catch (e: IOException) {
+                    status1?.text = "에러"
                 }
-            } catch (e: XmlPullParserException) {
-                status1?.text = "에러"
-            } catch (e: IOException) {
-                status1?.text = "에러"
+
             }
-            return@setOnClickListener
         }
-      return
-    }
+
 
     companion object {
         /**
